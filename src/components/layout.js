05f84a92e1d4d7ec6df9bb3a40738f21df8d9a3b
Ninel <inel2008@bk.ru>
2021-08-09 03:12:07 +0500

    change index.js, add layout, and global vars

---
+++ src/components/layout.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Header from '../pages/header'
+import { createGlobalStyle } from "styled-components"
+import "@fontsource/heebo/400.css"
+import "@fontsource/heebo/500.css"
+import "@fontsource/merienda-one/400.css"
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    html {
+        --color-primary: #1A7F72;
+        --text-primary: #000;
+        --text-secondary: rgba(#000, 0.5);
+    }
+
+    main {
+        max-width: 1620px;
+        margin: 0 auto;
+    }
+`
+
+const Layout = () => {
+    return (
+        <main>
+            <GlobalStyles />
+            <Header></Header>
+        </main>
+    )
+}
+
+export default Layout
