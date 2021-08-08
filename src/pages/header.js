863fa9ffd83c788b37adfb3b0884670d3dd9d0bb
Ninel <inel2008@bk.ru>
2021-08-09 03:12:49 +0500

    add header with logo and menu

---
+++ src/pages/header.js
@@ -0,0 +1,103 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Logo from '../images/logo.svg';
+import styled from "styled-components"
+
+const Wrapper = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 6px 0;
+`
+
+const Menu = styled.ul`
+    display: flex;
+    list-style: none;
+`
+
+const MenuItem = styled.li`
+    position: relative;
+    padding: 0 15px;
+    margin-right: 80px;
+
+    &:last-child {
+        margin-right: 0;
+    }
+
+    ::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -32px;
+        height: 10px;
+        background: var(--color-primary);
+        border-radius: 10px 10px 0px 0px;
+        opacity: 0;
+    }
+
+    a {
+        text-decoration: none;
+        font-family: "Heebo";
+        color: var(--text-primary);
+    }
+
+    &:hover {
+        ::after {
+            opacity: 1;
+        }
+        
+        a {
+            color: var(--color-primary);
+        }
+    }
+`
+
+// data
+const links = [
+    {
+        text: "Home",
+        url: "/",
+    },
+    {
+        text: "Explore",
+        url: "/explore",
+    },
+    {
+        text: "Classes",
+        url: "/classes",
+    },
+    {
+        text: "About Us",
+        url: "/about-us",
+    },
+    {
+        text: "Privacy",
+        url: "/privacy",
+    },
+    {
+        text: "Services",
+        url: "/services",
+    },
+]
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <img width="148px" height="119px" src={Logo} alt="logo"></img>
+            <nav>
+                <Menu>
+                    {links.map(link => (
+                        <MenuItem>
+                            <Link key={link.url} to={link.url}>
+                                {link.text}
+                            </Link>
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </nav>
+        </Wrapper>
+    )
+}
+
+export default Header
