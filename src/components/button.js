86fc182573b8df3fd11ec2f7f579e2316a1558cc
Ninel <inel2008@bk.ru>
2021-08-10 03:09:25 +0500

    chore: add button component with two types of button

---
+++ src/components/button.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Btn = styled.button`
+    font-size: 24px;
+    padding: 12px 44px;
+    border: none;
+    color: white;
+    background: linear-gradient(180deg, #259385 0%, #159685 100%);
+    box-shadow: 0px 13px 28px rgba(26, 127, 114, 0.2);
+    border-radius: 30px;
+    cursor: pointer;
+
+    &.size-l {
+        font-size: 30px;
+        font-weight: bold;
+        padding: 18px 89px;
+    }
+`
+
+const Button = (props) => {
+    return (
+        <Btn className={props.isBig ? 'size-l' : ''}>
+            {props.text}
+        </Btn>
+    )
+}
+
+export default Button;
