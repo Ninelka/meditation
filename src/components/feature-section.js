ef17edffbe5099b1b9759c6ce8b587c95ea61ac8
Ninel <inel2008@bk.ru>
2021-08-23 01:41:58 +0500

    chore: make feature section adaptive

--- src/components/feature-section.js
+++ src/components/feature-section.js
@@ -17,6 +17,20 @@ const Container = styled.section`
 const Row = styled.div`
     display: flex;
     margin: 120px 0 40px;
+
+    @media screen and (max-width: 1920px) {
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+
+        img {
+            width: 50%;
+            height: 50%;
+        }
+    }
+
+    @media screen and (max-width: 767px) {
+        margin: 60px 0 20px;
+    }
 `
 const CardContainer = styled.div`
     background-image: url(${props => props.bg});
@@ -30,6 +44,19 @@ const CardContainer = styled.div`
     :last-child {
         margin-right: 0;
     }
+
+    @media screen and (max-width: 1920px) {
+        margin-right: 0;
+    }
+
+    @media screen and (max-width: 1023px) {
+        background-color: white;
+        border-radius: 20px;
+        background-image: none;
+        min-width: 250px;
+        min-height: auto;
+        margin-bottom: 40px;
+    }
 `
 
 const Card = styled.div`
@@ -37,6 +64,23 @@ const Card = styled.div`
     flex-direction: column;
     align-items: center;
     padding: 80px 0;
+
+    @media screen and (max-width: 1920px) {
+
+        img {
+            width: 50px;
+            height: 50px;
+        }
+    }
+
+    @media screen and (max-width: 767px) {
+        padding: 30px 0;
+
+        img {
+            width: 30px;
+            height: 30px;
+        }
+    }
 `
 
 
@@ -47,6 +91,11 @@ const Title = styled.h3`
     font-size: 32px;
     line-height: 47px;
     margin: 20px 0;
+
+    @media screen and (max-width: 767px) {
+        font-size: 24px;
+        line-height: 24px;
+    }
 `
 
 const Text = styled.p`
@@ -57,6 +106,15 @@ const Text = styled.p`
     text-align: center;
     margin-bottom: 60px;
     max-width: 324px;
+
+    @media screen and (max-width: 1920px) {
+        margin: 0 12px 20px;
+    }
+
+    @media screen and (max-width: 767px) {
+        font-size: 14px;
+        max-width: 150px;
+    }
 `
 
 const data = [
