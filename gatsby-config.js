2c729c87e6dd38b9f008e873457c82ff22ba5c11
Ninel <inel2008@bk.ru>
2021-08-28 01:04:45 +0500

    fix: set correct siteUrl in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://meditationdev.gtsb.io/",
     title: "Meditation",
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud", "gatsby-plugin-react-helmet"],
